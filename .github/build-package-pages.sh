9ede3cd6adb1eee058f7e4b31f5d188e06d81b41
neil <neil@neils-MacBook-Pro.local>
2022-10-14 22:13:13 +0800

    add slug to packages.json

--- .github/build-package-pages.sh
+++ .github/build-package-pages.sh
@@ -6,12 +6,12 @@ for row in $(cat $1 | jq -r '.[] | @base64'); do
     _jq() {
      echo ${row} | base64 --decode | jq -r ${1}
     }
-   touch $2/$(_jq '.name').md
+   touch $2/$(_jq '.slug').md
    content="---
 type: page
 title: \"$(_jq '.name')\"
 Description: \"$(_jq '.desc')\"
 layout: \"package-detail\"
 ---"
-   echo "$content" > $2/$(_jq '.name').md
+   echo "$content" > $2/$(_jq '.slug').md
 done
