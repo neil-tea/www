7b85af805c831af3e125d84d7798fdad57839f16
neil <neil@neils-MacBook-Pro.local>
2022-10-18 13:31:32 +0800

    try write

--- lambdas/src/functions/hello/handler.ts
+++ lambdas/src/functions/hello/handler.ts
@@ -4,11 +4,42 @@ import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
 
+import AWS from 'aws-sdk/global';
+import S3 from 'aws-sdk/clients/s3';
+const  s3 = new S3();
+
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  await writeData();
   return formatJSONResponse({
     message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
     event,
   });
 };
+
+
+const writeData = async () => {
+  try {
+
+    const obj = {
+        firstname: "Navjot",
+        lastname: "Dhanawat"
+    };
+  
+    const buf = Buffer.from(JSON.stringify(obj));
+  
+    const data = {
+        Bucket: 'mockdist.tea.xyz',
+        Key: 'packages.json',
+        Body: buf,
+        ContentEncoding: 'base64',
+        ContentType: 'application/json',
+        ACL: 'public-read'
+    };
+  
+    await s3.upload(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export const main = middyfy(hello);
