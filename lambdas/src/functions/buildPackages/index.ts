0980ac1ea4c63454bee65e9d8082a0b5ca85fc36
neil <neil@neils-MacBook-Pro.local>
2022-10-20 10:36:04 +0800

    test: if ssm permission allows /parameter/ prefix

--- lambdas/src/functions/buildPackages/index.ts
+++ lambdas/src/functions/buildPackages/index.ts
@@ -7,7 +7,7 @@ export default {
   environment: {
     AIRTABLE_API_KEY: '${ssm:/lambdas/airtable/api_key}',
     AIRTABLE_PACKAGES_BASE: '${ssm:/lambdas/airtable/packages_base}',
-    AWS_DIST_BUCKET: '${ssm:/lambdas/dist_bucket}',
+    AWS_DIST_BUCKET: '${ssm:/parameter/lambdas/dist_bucket}',
     ALGOLIA_APP_ID: '${ssm:/lambdas/algolia/app_id}',
     ALGOLIA_SEARCH_API_KEY: '${ssm:/lambdas/algolia/search_api_key}',
   }
